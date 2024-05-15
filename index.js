4e2eb2bec9fc594ad2b0ef6bc47af2ab34513ef6
k12amrul <kamrul00islam0@gmail.com>
2024-05-15 20:30:42 +0800

    updated

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.use(cookieParser())
 console.log(process.env.DB_USER)
 console.log(process.env.DB_PASS)
 
+
+
+
 // 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uftqkre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -35,11 +38,94 @@ const client = new MongoClient(uri, {
   }
 });
 
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
+
 async function run() {
   try {
 
 
     const jobsCollection = client.db("jobDB").collection("jobs");
+
+
+
+    const logger = async (req, res, next) => {
+      console.log('called : ', req.host, req.originalUrl)
+
+      next()
+
+    }
+    const verifyToken = async (req, res, next) => {
+      const token = req.cookies?.token
+      console.log('  token in middleware ', token)
+      if (!token) {
+        return res.status(401).send({ message: 'not authorized ' })
+      }
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        console.log(err)
+        if (err) {
+          return res.status(401).send({ message: 'un authorized ' })
+
+        }
+        console.log(' value in token ', decoded)
+
+        req.user = decoded
+
+        next()
+      })
+      //  token asena 
+      //  req.userJwt= decoded
+
+      // next()
+
+    }
+
+    app.post('/jwt', logger, (req, res) => {
+      // console.log( process.env.ACCESS_TOKEN_SECRET)
+      const user = req.body
+      // console.log("user for token", user)
+      // console.log( process.env.ACCESS_TOKEN_SECRET)
+      const token = jwt.sign({
+        user
+      }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+      console.log({token :token} )
+      res
+        .cookie('token', token,cookieOptions 
+        //  {
+        //   httpOnly: true,
+        //   secure: false,
+        //   //  sameSite: 'none'
+        // }
+      )
+        .send({ success: true })
+
+
+
+
+    })
+
+
+
+
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body
+      console.log('log out ', user)
+
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+
+    })
+
+
+
+
+
 
 
     app.get('/jobs', async (req, res) => {
@@ -49,7 +135,7 @@ async function run() {
 
     })
 
-    app.get('/job/:id', async (req, res) => {
+    app.get('/job/:id', logger, verifyToken, async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await jobsCollection.findOne(query)
@@ -58,7 +144,7 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/myjobs/:email', async (req, res) => {
+    app.get('/myjobs/:email', logger, verifyToken, async (req, res) => {
       const email = req.params.email
 
       const query = { loggedInUserEmail: email }
@@ -110,6 +196,17 @@ async function run() {
       res.send(result)
     })
 
+
+
+    app.post( '/applyJob',async( req ,res )  =>{ 
+
+      const appliedJob= req.body
+      const result = await jobsCollection.insertOne(appliedJob )
+
+      res.send( result)
+
+
+    } )
 
 
 
